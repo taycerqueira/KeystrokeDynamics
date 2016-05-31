14a8f545ba9eea56ff57035822b54d82a5e58fd2
Tayane <engcomp.tayane@gmail.com>
2016-05-31 12:06:05 -0300

    adicionei a tela

--- src/uefs/Main.java
+++ src/uefs/Main.java
@@ -1,9 +1,13 @@
 package uefs;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Janela janela = new Janela();
+		janela.setVisible(true);
 
 	}
 
